593f1f02a4726aeb7057e8837551b60146ab5cd1
NickMileshkin <mileshkin.nikita@yandex.ru>
2021-06-25 20:05:17 +0700

    add client.py, Stock_plot.py and ui_MainWindow.py

---
+++ Stock_plot.py
@@ -0,0 +1,54 @@
+import sys
+import matplotlib
+matplotlib.use('Qt5Agg')
+import apimoex
+import requests
+import pandas as pd
+from PyQt5 import QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+import matplotlib.dates
+
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+
+class Stock_plot(QtWidgets.QMainWindow):
+
+    def __init__(self, ticket, firm_name, start_time, end_time):
+        super(Stock_plot, self).__init__()
+        with requests.Session() as session:
+            share_json = apimoex.get_board_candles(session, ticket, interval=24, start=start_time, end=end_time)
+            share_df = pd.DataFrame(share_json)
+        dates = []
+        for index, row in share_df.iterrows():
+            dates.append(row["begin"].split(' ')[0])
+        date_float = matplotlib.dates.date2num(dates)
+        sc = MplCanvas(self, width=12, height=5, dpi=100)
+        sc.axes.plot(date_float, share_df['open'])
+        sc.axes.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%Y-%m-%d"))
+        sc.axes.set_title(f"Цена акций {firm_name} с {start_time} по {end_time}")
+        sc.axes.set_ylabel("Цена акций в рублях")
+
+        toolbar = NavigationToolbar(sc, self)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(toolbar)
+        layout.addWidget(sc)
+
+        # Create a placeholder widget to hold our toolbar and canvas.
+        widget = QtWidgets.QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        self.show()
+
+
+app = QtWidgets.QApplication(sys.argv)
+w = Stock_plot("GAZP", "'Газпром'", "2020-05-20", "2021-06-24")
+app.exec_()
---
+++ client.py
@@ -0,0 +1,116 @@
+import sys  # sys нужен для передачи argv в QApplication
+import requests.exceptions
+from PyQt5 import QtWidgets, QtCore, QtGui
+from ui_MainWindow import Ui_MainWindow
+import pandas as pd
+import apimoex
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.active_share = None
+        self.shares = []
+        self.add_share()
+        self.pushButton.clicked.connect(self.find_shares)
+
+    def find_shares(self):
+        share_name = self.lineEdit.text()
+
+        if share_name != '':
+            for i in reversed(range(self.scrollLayout_shares.count())):
+                widgetToRemove = self.scrollLayout_shares.itemAt(i).widget()
+                self.scrollLayout_shares.removeWidget(widgetToRemove)
+                widgetToRemove.setParent(None)
+
+            for i in range(len(self.shares)):
+                if share_name in self.shares[i].firm_name:
+                    self.scrollLayout_shares.addRow(self.shares[i])
+        else:
+            for i in reversed(range(self.scrollLayout_shares.count())):
+                widgetToRemove = self.scrollLayout_shares.itemAt(i).widget()
+                self.scrollLayout_shares.removeWidget(widgetToRemove)
+                widgetToRemove.setParent(None)
+
+            for i in range(len(self.shares)):
+                self.scrollLayout_shares.addRow(self.shares[i])
+
+    def add_share(self):
+        with requests.Session() as session:
+            data = apimoex.get_board_securities(session, columns=("SECID", "SECNAME", "SHORTNAME"))
+            data = pd.DataFrame(data)
+            data.sort_values("SECNAME")
+        for index, row in data.iterrows():
+            s = Share(row["SECID"], row["SHORTNAME"])
+            s.clicked.connect(s.open_shares)
+            self.shares.append(s)
+            self.scrollLayout_shares.addRow(s)
+
+
+class ClickableWidget(QtWidgets.QWidget):  # класс для виджетов, на которые можно нажимать
+    clicked = QtCore.pyqtSignal()
+
+    def mousePressEvent(self, QMouseEvent):
+        self.clicked.emit()
+        QtWidgets.QWidget.mousePressEvent(self, QMouseEvent)
+
+
+class Share (ClickableWidget):
+    def __init__(self, ticket, name):
+        super(Share, self).__init__()
+        self.ticket = ticket
+        self.url = f"https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/{self.ticket}.json"
+        self.data = []
+        self.plot_data = []
+        self.plot_time = []
+        self.firm_name = name
+        self.name = QtWidgets.QLabel(self.firm_name)
+        self.container = QtWidgets.QWidget(self)
+        self.container.setGeometry(0, 0, 160, 50)
+        self.layout = QtWidgets.QVBoxLayout(self.container)
+        self.layout.addWidget(self.name)
+        self.container.setStyleSheet("background-color:white;")
+        self.setLayout(self.layout)
+
+    def update(self):
+        r = requests.get(self.url).json()
+        self.data = []
+        self.plot_data.append(r['marketdata']['data'][0][4])
+        self.plot_time.append(r['marketdata']['data'][0][32])
+        self.data.append(r['securities']['data'][0][9])
+        self.data.append(str(r['marketdata']['data'][0][12]))
+        self.data.append(str(r['marketdata']['data'][0][25]))
+        self.data.append(str(r['marketdata']['data'][0][4]))
+        self.data.append(str(r['marketdata']['data'][0][2]))
+        self.data.append(str(r['marketdata']['data'][0][11]))
+        self.data.append(str(r['marketdata']['data'][0][10]))
+
+
+    def open_shares(self):
+        if main_window.active_share != self:
+            if main_window.active_share != None:
+                main_window.active_share.container.setStyleSheet("background-color:white;")
+            main_window.active_share = self
+            self.update()
+
+            self.container.setStyleSheet("background-color:rgb(183, 242, 255);")
+            main_window.lineEdit.setText("")
+            main_window.find_shares()
+
+            main_window.label_firm_name.setText(self.data[0])
+            main_window.label_8.setText(self.ticket)
+            main_window.label_cost.setText(self.data[1])
+
+            if self.data[2][0] == "-":
+                main_window.label_dif_precent.setStyleSheet("color:red;")
+                main_window.label_dif_precent.setText(f"{self.data[2]}%")
+            else:
+                main_window.label_dif_precent.setText(f"+{self.data[2]}%")
+                main_window.label_dif_precent.setStyleSheet("color:green;")
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    app.exec_()
